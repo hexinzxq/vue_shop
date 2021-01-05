21073f7f83a9cc6197d65775e3cd6144e817d8c2
hexinzxq <73218041+hexinzxq@users.noreply.github.com>
2021-01-05 10:09:29 +0800

    完成登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -71,30 +71,30 @@ export default {
           },
         ],
       },
-    };
+    }
   },
-  methods:{
+  methods: {
     //   点击重置重置登录表单
-      resetLoginForm(){
-          this.$refs.loginFormRef.resetFields();
-      },
-      login(){
-          this.$refs.loginFormRef.validate(async (valid)=>{
-              if(!valid) return;
-              const {data:res} =await this.$http.post('login',this.loginForm)
-            //   console.log(res);
-            if(res.meta.status != 200) return this.$message.error('登陆失败');
-            this.$message.success('登陆成功');
-            //1.将登录成功之后的token，保存到客户端的sessionStorage中
-            //1.1项目中除了登陆之外的其他API接口，必须在登录之后才能访问
-            //1.2token只应当在当前网站打开期间生效，所以将token保存在sessionStorage中
-            window.sessionStorage.setItem('token',res.data.token);
-            //2.通过编程式导航跳转到后台主页，路由地址是/home
-            this.$router.push('/home');
-          })
-      }
-  }
-};
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return
+        const { data: res } = await this.$http.post("login", this.loginForm)
+        //   console.log(res);
+        if (res.meta.status != 200) return this.$message.error("登陆失败")
+        this.$message.success("登陆成功")
+        //1.将登录成功之后的token，保存到客户端的sessionStorage中
+        //1.1项目中除了登陆之外的其他API接口，必须在登录之后才能访问
+        //1.2token只应当在当前网站打开期间生效，所以将token保存在sessionStorage中
+        window.sessionStorage.setItem("token", res.data.token)
+        //2.通过编程式导航跳转到后台主页，路由地址是/home
+        this.$router.push("/home")
+      })
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
